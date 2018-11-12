8e004a8d6e09e769d41420dc733a3621d403e6bc
Nikolay <evrinoma@gmail.com>
2018-11-12 18:19:10 +0300

    rebase and little fix readme
    Signed-off-by: Nikolay <evrinoma@gmail.com>

--- PHP/TEST1/page.test.php
+++ PHP/TEST1/page.test.php
@@ -1,4 +1,4 @@
-<?php 
+<?php /* $Id */
 if (!defined('SITE_IS_AUTH')) {
     die('No direct script access allowed');
 }
@@ -10,48 +10,43 @@ function calcTime($timeStart, $timeEnd, $isClass = true)
     echo '<b>Total Execution '.(($isClass) ? 'in class' : 'in funct').' Time:</b> '.sprintf("%f", ($timeEnd - $timeStart)).'<br>';
 }
 
-$timeStart = microtime(true);
-$test1     = new Test1(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']);
-$timeEnd   = microtime(true);
-(!defined('IS_TIME_TEST')) ? $test1->print_r() : calcTime($timeStart, $timeEnd);
+$aTest1 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
 
-
-$timeStart = microtime(true);
-$result    = [];
-array_filter(
-    ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'],
-    function ($value) use (&$result) {
-        $result = (!$result) ? array($value => null) : array($value => $result);
-    }
-);
-$timeEnd = microtime(true);
-(!defined('IS_TIME_TEST')) ? print_r($result) : calcTime($timeStart, $timeEnd, false);
-
-
-$timeStart = microtime(true);
-$test2     = new Test2(
-    [
+$aTest2 =     [
         'parent.child.field'      => 1,
         'parent.child.field2'     => 2,
         'parent2.child.name'      => 'test',
         'parent2.child2.name'     => 'test',
         'parent2.child2.position' => 10,
         'parent3.child3.position' => 10,
-    ]
-);
+    ];
+print_r($aTest1);
+$timeStart = microtime(true);
+$test1     = new Test1($aTest1);
 $timeEnd   = microtime(true);
-(!defined('IS_TIME_TEST')) ? $test2->printR() : calcTime($timeStart, $timeEnd);
-
+(!defined('IS_TIME_TEST')) ? $test1->print_r() : calcTime($timeStart, $timeEnd);
+echo "<br>"; 
 $timeStart = microtime(true);
 $result    = [];
-$data1     = [
-    'parent.child.field'      => 1,
-    'parent.child.field2'     => 2,
-    'parent2.child.name'      => 'test',
-    'parent2.child2.name'     => 'test',
-    'parent2.child2.position' => 10,
-    'parent3.child3.position' => 10,
-];
+array_filter(
+    $aTest1,
+    function ($value) use (&$result) {
+        $result = (!$result) ? array($value => null) : array($value => $result);
+    }
+);
+$timeEnd = microtime(true);
+(!defined('IS_TIME_TEST')) ? print_r($result) : calcTime($timeStart, $timeEnd, false);
+echo "<br>";
+
+print_r($aTest2);
+$timeStart = microtime(true);
+$test2     = new Test2($aTest2);
+$timeEnd   = microtime(true);
+(!defined('IS_TIME_TEST')) ? $test2->printR() : calcTime($timeStart, $timeEnd);
+echo "<br>";
+$timeStart = microtime(true);
+$result    = [];
+$data1     = $aTest2;
 array_walk(
     $data1,
     function ($valueData, $key) use (&$result) {
@@ -67,7 +62,7 @@ array_walk(
 );
 $timeEnd = microtime(true);
 (!defined('IS_TIME_TEST')) ? print_r($result) : calcTime($timeStart, $timeEnd, false);
-
+echo "<br>";
 
 $timeStart = microtime(true);
 $test2->getReverse();
@@ -90,3 +85,4 @@ $callReverse = function ($data, $key) use (&$callReverse, &$keyString, &$convert
 array_walk($result, $callReverse);
 $timeEnd = microtime(true);
 (!defined('IS_TIME_TEST')) ? print_r($converted) : calcTime($timeStart, $timeEnd, false);
+echo "<br>";
---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>Часть 1</h1><img src="https://raw.githubusercontent.com/evrinoma/Job-Tests/master/img/TEST1/p1.png">
+<h1>Часть 2</h1><img src="https://raw.githubusercontent.com/evrinoma/Job-Tests/master/img/TEST1/p2.png">
+<h1>Часть 3</h1><img src="https://raw.githubusercontent.com/evrinoma/Job-Tests/master/img/TEST1/p3.png">
